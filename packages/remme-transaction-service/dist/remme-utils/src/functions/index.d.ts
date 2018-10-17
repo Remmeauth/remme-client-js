6dc30e3dd19e9e15a053d465a48bbf37b3d13795
ArtemFrantsiian <artemfrantsiian@gmail.com>
2018-10-17 18:35:21 +0300

    refactor remme-utils

---
+++ packages/remme-transaction-service/dist/remme-utils/src/functions/index.d.ts
@@ -0,0 +1,10 @@
+export declare const sha512: (value: string) => string;
+export declare const hexToBytes: (str: string) => Uint8Array;
+export declare const bytesToHex: (uint8arr: Uint8Array) => string;
+export declare const base64ToArrayBuffer: (base64: string) => Uint8Array;
+export declare const utf8ToBytes: (str: string) => any;
+export declare const toHex: (str: string) => string;
+export declare const getAddressFromData: (familyName: string, data: string) => string;
+export declare const toHexString: (byteArray: any) => string;
+export declare const toUTF8Array: (str: any) => any[];
+export declare const makeSettingsAddress: (key: string) => string;
