7ee23eb7f73069b331c83815b06042b09191841c
ArtemFrantsiian <artemfrantsiian@gmail.com>
2018-11-20 15:37:10 +0200

    v0.5.0-0

--- packages/remme-account/src/models/index.ts
+++
@@ -1,4 +0,0 @@
-import { Secp256k1PrivateKey, Secp256k1PublicKey } from "sawtooth-sdk/signing/secp256k1";
-
-export type RemmeAccountPrivateKey = Secp256k1PrivateKey;
-export type RemmeAccountPublicKey = Secp256k1PublicKey;
