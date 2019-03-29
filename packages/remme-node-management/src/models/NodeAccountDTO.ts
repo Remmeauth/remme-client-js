681c02a09aee8f62f5e0f1f0f4919d65b08a0256
Oleksandr Soloviov <olksns@gmail.com>
2019-03-29 12:07:33 +0200

    Add default configuration;

--- packages/remme-node-management/src/models/NodeAccountDTO.ts
+++ packages/remme-node-management/src/models/NodeAccountDTO.ts
@@ -10,12 +10,19 @@ export class NodeAccountDTO {
         unfrozen: number;
     };
 
+    private _reputationDefault = {
+        frozen: 0,
+        unfrozen: 0,
+    };
+
     constructor({ node_state, balance, reputation }: INodeAccountResponse) {
         this.state = node_state;
-        this.reputation = {
-            frozen: parseInt(reputation.frozen, 10),
-            unfrozen: parseInt(reputation.unfrozen, 10),
-        };
+        this.reputation = reputation
+            ? {
+                frozen: parseInt(reputation.frozen, 10),
+                unfrozen: parseInt(reputation.unfrozen, 10),
+              }
+            : this._reputationDefault;
         this.balance = parseInt(balance, 10);
     }
 }
