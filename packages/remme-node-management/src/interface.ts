d90d9fb3aea0c1b562e0d182dd021be249cab28e
Oleksandr Soloviov <olksns@gmail.com>
2019-04-15 16:49:48 +0300

    Update protobufs in JS about NodeAccount; Add new fields in NodeAccount in JS library;

--- packages/remme-node-management/src/interface.ts
+++ packages/remme-node-management/src/interface.ts
@@ -1,13 +1,12 @@
 import { IBaseTransactionResponse } from "remme-transaction-service";
-import { ISetBetPayload } from "remme-protobuf";
-import { NodeAccount, NodeInfo, NodeConfig } from "./models";
+import { NodeAccount, NodeInfo, NodeConfig, BetType } from "./models";
 
 export interface IRemmeNodeManagement {
 
     openNode(): Promise<IBaseTransactionResponse>;
     openMasterNode(initialStake: number): Promise<IBaseTransactionResponse>;
     closeMasterNode(): Promise<IBaseTransactionResponse>;
-    setBet(payload: ISetBetPayload): Promise<IBaseTransactionResponse>;
+    setBet(betType: BetType | number): Promise<IBaseTransactionResponse>;
     getInitialStake(): Promise<number>;
     getNodeInfo(): Promise<NodeInfo>;
     getNodeAccount(nodeAccountAddress: string): Promise<NodeAccount>;
